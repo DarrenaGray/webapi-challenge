b2e1772b8432489a0aa2919b41026f7fe306ff3e
Darrena Gray <40064931+WhatTheSpork@users.noreply.github.com>
2019-04-26 13:36:19 -0400

    Created endpoint for GET request by Id for retrieving specific actions. Need to fix the logic for http request

--- data/helpers/actionRouter.js
+++ data/helpers/actionRouter.js
@@ -18,6 +18,27 @@ router.get('/', (req, res) => {
         });
 });
 
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+    Action
+        .get(id)
+        .then(action => {
+            if (action.length !== 0) {
+                res.status(200).json(action);
+            } else {
+                res.status(404).json({
+                    message: "The action with the specified ID does not exist."
+                });
+            };
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err,
+                message: "The action information could not be retrieved."
+            })
+        });
+});
+
 
 
 module.exports = router;
