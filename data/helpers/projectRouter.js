188d6cb47e14d5ba4ba12d039053f78584b4a475
Darrena Gray <40064931+WhatTheSpork@users.noreply.github.com>
2019-04-26 12:47:59 -0400

    Created endpoint POST request for adding a new project to database. Changed/fixed GET request endpoint on retrieving projects

--- data/helpers/projectRouter.js
+++ data/helpers/projectRouter.js
@@ -4,24 +4,34 @@ const Project = require('./projectModel');
 
 const router = express.Router();
 
-router.get('/:id', (req, res)=> {
+router.get('/', (req, res)=> {
     const { id } = req.params;
     Project
         .get(id)
-        .then(project => {
-            if(project) {                
-                res.status(200).json(project)
-            } else {
-                res.status(404).json({
-                    message: "The project with the specified ID does not exist."
-                })
-            }
+        .then(projects => {           
+                res.status(200).json(projects)
         })
         .catch(err => {
             res.status(500).json({
+                error: err,
                 errorMessage: "The project could not be retrieved."
-            })
+            });
+        });
+});
+
+router.post('/', (req, res) => {
+    const projectInfo = req.body;
+    Project
+        .insert(projectInfo)
+        .then(project => {
+            res.status(201).json(project);
         })
-})
+        .catch(err => {
+            res.status(500).json({
+                error: err,
+                errorMessage: "There was a problem saving this project."
+            });
+        });
+});
 
 module.exports = router;
